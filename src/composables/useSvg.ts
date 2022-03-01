6236cc28b64bfba5c70055b3d34623b6b692582f
iliagromov <ilia.workproffile@gmail.com>
2022-03-01 11:26:57 +0300

    Поправил обновление шрифта на макете

--- src/composables/useSvg.ts
+++ src/composables/useSvg.ts
@@ -8,40 +8,40 @@ export default function useSvg() {
     // layoutFieldText,
     backgroundFilename,
     layoutFieldsText,
+    layoutFieldTextFont,
     layoutFieldTextSize,
     layoutFieldTextColor,
   } = useStoreDesignConfig();
 
   const svgText = computed(() => layoutFieldsText.value.map((text) => (text.innerText)));
   const bgSrc = computed(() => (backgroundFilename.value ? backgroundFilename.value : ''));
+  const fontFamily = computed(() => layoutFieldTextFont.value.map((size) => size.styles.font));
   const fontSizes = computed(() => layoutFieldTextSize.value.map((size) => (`${size.styles.size}em`)));
   const fontColors = computed(() => layoutFieldTextColor.value.map((color) => (
     `color: ${color.styles.color}`)));
   // каждый раз приходится парсить svg
-  // можно сделать компьютед?
-  // каждый раз запрашиваю bg и деалю парсиннг, может хранить svg в store?
-  // шрифт в свг обновляется не сразу,..
   const renderSVG = async () => {
-    const response = await fetch(bgSrc.value);
-    const responseText = await response.text();
-    const parser = new DOMParser();
-    const svg = parser.parseFromString(responseText, 'image/svg+xml');
+    if (bgSrc.value) {
+      const response = await fetch(bgSrc.value);
+      const responseText = await response.text();
+      const parser = new DOMParser();
+      const svg = parser.parseFromString(responseText, 'image/svg+xml');
 
-    svg.querySelectorAll('text').forEach((item, index:number) => {
-      // fixme: layoutFieldsText.value[index]
-      item.textContent = layoutFieldsText.value[index] ? layoutFieldsText.value[index].innerText : '';
-      item.style.fontSize = layoutFieldsText.value[index] ? `${layoutFieldsText.value[index].styles.size}em` : '100em';
-      item.style.fill = layoutFieldsText.value[index] ? layoutFieldsText.value[index].styles.color : '#000000';
-      item.style.fontFamily = layoutFieldsText.value[index] ? layoutFieldsText.value[index].styles.font : 'AdventureC';
-    });
+      svg.querySelectorAll('text').forEach((item, index:number) => {
+        // fixme: layoutFieldsText.value[index]
+        item.textContent = layoutFieldsText.value[index] ? layoutFieldsText.value[index].innerText : '';
+        item.style.fontSize = layoutFieldsText.value[index] ? `${layoutFieldsText.value[index].styles.size}em` : '100em';
+        item.style.fill = layoutFieldsText.value[index] ? layoutFieldsText.value[index].styles.color : '#000000';
+        item.style.fontFamily = layoutFieldsText.value[index] ? layoutFieldsText.value[index].styles.font : 'AdventureC';
+      });
 
-    modelSvgIframe.value = svg.documentElement.outerHTML;
+      modelSvgIframe.value = svg.documentElement.outerHTML;
+    }
   };
 
-  // watch изменения текста, шрифта, размера
   watch(backgroundFilename, renderSVG);
-  // watch(layoutFieldText, renderSVG);
   watch(svgText, renderSVG);
+  watch(fontFamily, renderSVG);
   watch(fontSizes, renderSVG);
   watch(fontColors, renderSVG);
 
